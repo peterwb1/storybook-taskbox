fa3a06a42bbce16fa32165e1524a3d1c8cc94c6e
peter <peterwball1@gmail.com>
2024-10-23 15:32:02 +0100

    screens before play function use

--- taskbox/vite.config.js
+++ taskbox/vite.config.js
@@ -10,5 +10,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 3000,
+    https: {
+      key: './certs/localhost-key.pem',
+      cert: './certs/localhost.pem'
+    }
   }
 })

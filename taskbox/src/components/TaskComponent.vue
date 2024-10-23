3fa6baf401700f51782f11834c17a3306d2dd4e7
peter <peterwball1@gmail.com>
2024-10-23 11:56:07 +0100

    feat: Add TaskComponent.vue and Task.stories.ts

---
+++ taskbox/src/components/TaskComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="list-item">
+      <label for="title" :aria-label="task.title">
+        <input type="text" readonly :value="task.title" id="title" name="title" />
+      </label>
+    </div>
+  </template>
+  
+<script setup lang="ts">
+  defineProps({
+    task: {
+      type: Object,
+      required: true,
+      default: () => ({ id: '', state: '', title: '' }),
+      validator: (task) => ['id', 'state', 'title'].every((key) => key in task)
+    }
+  });
+</script>
